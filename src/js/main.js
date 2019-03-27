559cc4118988ad2e899cfe55f3ab7efeb40b277b
Lucas Flaquer <lucas.flaquer@vm2.com.br>
2019-03-27 15:15:23 -0300

    menu responsivo

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,30 @@
-// js code here
+window.onload = function() {
+	menuMobile();
+}
+
+
+function menuMobile() {
+	let i;
+	let headerMobile = document.createElement('div');
+	let header = document.querySelector('.header');
+	let headerItems = document.querySelectorAll('.header > *');
+	headerMobile.classList.add('header--mobile');
+	if (window.innerWidth < 767) {
+		console.log('estrutura nova do menu');
+		console.log(headerMobile);
+		for (i=0; i<headerItems.length; i++) {
+			switch( true ){
+				case headerItems[i].classList.contains('header--logo'):
+					headerMobile.append(headerItems[i]);
+					break;
+    		case headerItems[i].classList.contains('header--hamburger'):
+     		 	headerMobile.append(headerItems[i]);
+    		  break;
+    		case headerItems[i].classList.contains('header--bag'):
+     			headerMobile.append(headerItems[i]);
+    		  break;      
+  		}
+		}
+		header.prepend(headerMobile);
+	}
+}
