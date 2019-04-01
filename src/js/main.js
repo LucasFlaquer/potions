0c30af39b59956bc6f1f17104b497737725c4673
Lucas Flaquer <lucas.flaquer@vm2.com.br>
2019-04-01 11:58:26 -0300

    inicio do menu mobile

--- src/js/main.js
+++ src/js/main.js
@@ -5,26 +5,51 @@ window.onload = function() {
 
 function menuMobile() {
 	let i;
-	let headerMobile = document.createElement('div');
-	let header = document.querySelector('.header');
-	let headerItems = document.querySelectorAll('.header > *');
-	headerMobile.classList.add('header--mobile');
-	if (window.innerWidth < 767) {
-		console.log('estrutura nova do menu');
-		console.log(headerMobile);
-		for (i=0; i<headerItems.length; i++) {
-			switch( true ){
-				case headerItems[i].classList.contains('header--logo'):
-					headerMobile.append(headerItems[i]);
-					break;
-    		case headerItems[i].classList.contains('header--hamburger'):
-     		 	headerMobile.append(headerItems[i]);
-    		  break;
-    		case headerItems[i].classList.contains('header--bag'):
-     			headerMobile.append(headerItems[i]);
-    		  break;      
-  		}
-		}
-		header.prepend(headerMobile);
-	}
+	let menuMobile = document.createElement('div');
+	let hamburger = document.createElement('div');
+	let logo = document.querySelector('.header--logo');
+	let cart = document.querySelector('.header--bag');
+	let search = document.querySelector('.header--search');
+	let nav = document.querySelector('.header--menu');
+	hamburger.innerHTML = "<span></span><span></span><span></span>";
+	menuMobile.classList.add("header--mobile");
+	hamburger.classList.add("header--hamburger");	
+
+	menuMobile.appendChild(hamburger);
+	menuMobile.appendChild(logo);
+	menuMobile.appendChild(cart);
+	menuMobile.appendChild(search);
+	menuMobile.appendChild(nav);
+	console.log(hamburger);
+	console.log(menuMobile);
+	document.querySelector('.header').prepend(menuMobile);
+
+
+
+
+
+
+	// let i;
+	// let headerMobile = document.createElement('div');
+	// let header = document.querySelector('.header');
+	// let headerItems = document.querySelectorAll('.header > *');
+	// headerMobile.classList.add('header--mobile');
+	// if (window.innerWidth < 767) {
+	// 	console.log('estrutura nova do menu');
+	// 	console.log(headerMobile);
+	// 	for (i=0; i<headerItems.length; i++) {
+	// 		switch( true ){
+	// 			case headerItems[i].classList.contains('header--logo'):
+	// 				headerMobile.append(headerItems[i]);
+	// 				break;
+ //    		case headerItems[i].classList.contains('header--hamburger'):
+ //     		 	headerMobile.append(headerItems[i]);
+ //    		  break;
+ //    		case headerItems[i].classList.contains('header--bag'):
+ //     			headerMobile.append(headerItems[i]);
+ //    		  break;      
+ //  		}
+	// 	}
+	// 	header.prepend(headerMobile);
+	// }
 }
