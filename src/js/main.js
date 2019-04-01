2d6de1d3e739ef763235f1df38339b5f7b3d6c43
Lucas Flaquer <lucas.flaquer@vm2.com.br>
2019-04-01 15:02:52 -0300

    menu responsivo animacao

--- src/js/main.js
+++ src/js/main.js
@@ -25,10 +25,44 @@ function menuMobile() {
 }
 function toggleMenu() {
 	let menu = document.querySelector('.header--menu');
+	let logo = document.querySelector('.header--logo');
+	let bag = document.querySelector('.header--bag');
+	let search = document.querySelector('.header--search');
+	// menu.classList.add('animated');
 	document.querySelector('.header--hamburger').addEventListener('click', function() {
 		this.classList.toggle('open');
-		menu.classList.toggle('open');
-		
-	});
+		if (menu.classList.contains('open')) {
+			console.log('fechei');
+			// menu.classList.remove('open');
+			animateCSS(menu, 'fadeOut', function() {
+				menu.classList.remove('open');
+				console.log('test');
+			});
+		} else {
+			console.log('abri');
+			// menu.classList.add('open');
+			animateCSS(menu, 'fadeIn', function() {
+				console.log('test');
+			});
+			menu.classList.add('open');
+		}
 
+		// menu.classList.toggle('open');
+		logo.classList.toggle('hidden');
+		bag.classList.toggle('hidden');
+	});
+}
+
+function animateCSS(element, animationName, callback) {
+    // const node = document.querySelector(element)
+    element.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+        element.classList.remove('animated', animationName)
+        element.removeEventListener('animationend', handleAnimationEnd)
+        // console.log('test');
+        if (typeof callback === 'function') callback()
+    }
+
+    element.addEventListener('animationend', handleAnimationEnd)
 }
